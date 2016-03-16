7bf057e65c26691596e8bdb1b3faf4922a41266c
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-03-16 11:58:46 +0700

    First commit

---
+++ src/main/java/com/blockcipherexperiment/BlockCipherTools.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blockcipherexperiment;
+
+/**
+ *
+ * @author Rakhmatullah Yoga S
+ */
+public class BlockCipherTools {
+    private String key;
+    
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getKey() {
+        return this.key;
+    }
+    
+}
