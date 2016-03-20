c55c43bb30894fdc98909132b2b378e273c2f5bb
akhfa <akhmadfakhoni@gmail.com>
2016-03-20 17:25:15 +0700

    kerangka feistel ok

--- src/main/java/com/blockcipherexperiment/BlockCipherAlgorithm.java
+++ src/main/java/com/blockcipherexperiment/BlockCipherAlgorithm.java
@@ -8,12 +8,18 @@ package com.blockcipherexperiment;
 /**
  *
  * @author Rakhmatullah Yoga S
+ * 16 byte kunci
  */
 public class BlockCipherAlgorithm {
     private byte[] plain;
     private String key;
     private byte[] cipher;
 
+    public BlockCipherAlgorithm()
+    {
+        plain = Tools.stringToBytes("abcdefghijklmnop");
+    }
+    
     public byte[] getPlain() {
         return plain;
     }
@@ -36,5 +42,35 @@ public class BlockCipherAlgorithm {
 
     public void setCipher(byte[] cipher) {
         this.cipher = cipher;
+    }
+    
+    public void feistel()
+    {
+        // inisialisasi right left
+        byte [] Left = new byte[plain.length / 2];
+        byte [] Right = new byte[plain.length / 2];
+        System.arraycopy(this.plain, 0, Left, 0, plain.length / 2);
+        System.arraycopy(this.plain, plain.length / 2 , Right, 0, plain.length / 2);
+        
+        // Mulai feistel
+        for(int i = 0; i < 8; i++)
+        {
+            // simpan initial state
+            byte [] LeftTemp = new byte[Left.length];
+            System.arraycopy(Left, 0, LeftTemp, 0, Left.length);
+            byte [] RightTemp = new byte[Right.length];
+            System.arraycopy(Right, 0, RightTemp, 0, Right.length);
+            
+            // L = R-1
+            System.arraycopy(Right, 0, Left, 0, Right.length);
+            
+            // R = L-1  XOR  R-1
+            // Tambahkan fungsi F ke sini
+            for(int idxbyte = 0; idxbyte < Right.length; idxbyte++)
+            {
+                byte temp = (byte) (LeftTemp[idxbyte] ^ RightTemp[idxbyte]);
+                Right[idxbyte] = temp;
+            }
+        }
     }
 }
---
+++ src/main/java/com/blockcipherexperiment/Tools.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blockcipherexperiment;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author Andre
+ */
+public class Tools {
+    
+    public static int[] getShuffledInts(String strSeed, int min, int max){
+        // bentuk seed untuk random
+        long seed = 0;
+        for (int i = 0; i < strSeed.length(); i++)
+            seed += (long)strSeed.charAt(i);
+        
+        // buat array berisi angka dari min sd max
+        List<Integer> arrayToShuffle = new ArrayList<Integer>();
+        for (int i = min; i <= max; i++)
+            arrayToShuffle.add(i); 
+        
+        // acak-acak dengan seed yang diberikan
+        Collections.shuffle(arrayToShuffle, new Random(seed));
+        
+        // convert ke primitive
+        int shuffledInts[] = new int[max - min + 1];
+        for (int i = 0; i < shuffledInts.length; i++){
+            shuffledInts[i] = arrayToShuffle.get(i);
+        }
+        
+        return shuffledInts;
+    }
+    
+    // Konversi Boolean >< Byte
+    public static boolean[] convertToBoolArray(byte[] bytes) {
+        return Tools.convert(bytes, bytes.length * 8);
+    }
+    
+    public static byte[] convertToByte(boolean[] booleanOfData) {
+        int length = booleanOfData.length / 8;
+        int mod = booleanOfData.length % 8;
+        if(mod != 0){
+                ++length;
+        }
+        byte[] retVal = new byte[length];
+        int boolIndex = 0;
+        for (int byteIndex = 0; byteIndex < retVal.length; ++byteIndex) {
+                for (int bitIndex = 7; bitIndex >= 0; --bitIndex) {
+                        // Another bad idea
+                        if (boolIndex >= booleanOfData.length) {
+                                return retVal;
+                        }
+                        if (booleanOfData[boolIndex++]) {
+                                retVal[byteIndex] |= (byte) (1 << bitIndex);
+                        }
+                }
+        }
+        return retVal;
+    }
+    private static boolean[] convert(byte[] bits, int significantBits) {
+        boolean[] retVal = new boolean[significantBits];
+        int boolIndex = 0;
+        for (int byteIndex = 0; byteIndex < bits.length; ++byteIndex) {
+                for (int bitIndex = 7; bitIndex >= 0; --bitIndex) {
+                        if (boolIndex >= significantBits) {
+                                // Bad to return within a loop, but it's the easiest way
+                                return retVal;
+                        }
+
+                        retVal[boolIndex++] = (bits[byteIndex] >> bitIndex & 0x01) == 1 ? true
+                                        : false;
+                }
+        }
+        return retVal;
+    }
+    
+    public static void printMatriks(boolean [] matriks)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                System.err.print(matriks[i * 8 + j] == true? 1: 0);
+            }
+            System.err.println("");
+        }
+    }
+    
+    public static void printArray(boolean[] array)
+    {
+        for(boolean b : array)
+            System.err.print(b? 1:0);
+        System.err.println("");
+    }
+    
+    public static byte[] intToBytes(int value) {
+    return new byte[] {
+            (byte)(value >> 24),
+            (byte)(value >> 16),
+            (byte)(value >> 8),
+            (byte)value};
+    }
+    
+    public static int bytesToInt(byte[] bytes) {
+        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+    }
+    
+    public static String bytesToString(byte [] bytes)
+    {
+        return new String(bytes);
+    }
+    
+    public static byte [] stringToBytes(String string)
+    {
+        return string.getBytes();
+    }
+    
+    public static byte [] floatToByte(float value)
+    {
+        return ByteBuffer.allocate(4).putFloat(value).array();
+    }
+    
+    public static float bytesToFloat(byte [] value)
+    {
+        return ByteBuffer.wrap(value).getFloat();
+    }
+    
+    public static int oneByteToInt(boolean [] value)
+    {
+        int result = 0;
+        for(int i = value.length - 1; i >= 0; i--)
+        {
+            if(value[i])
+                result += Math.pow(2, 7 - i);
+        }
+        return result;
+    }
+}
