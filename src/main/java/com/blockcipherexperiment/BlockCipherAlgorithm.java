0c274d1f5d29901e7d377349c61c0c81d2f58326
akhfa <akhmadfakhoni@gmail.com>
2016-03-22 15:46:50 +0700

    encrypt decrypt ok

--- src/main/java/com/blockcipherexperiment/BlockCipherAlgorithm.java
+++ src/main/java/com/blockcipherexperiment/BlockCipherAlgorithm.java
@@ -5,8 +5,7 @@
  */
 package com.blockcipherexperiment;
 
-import java.util.Collections;
-import java.util.Random;
+import java.io.UnsupportedEncodingException;
 
 /**
  *
@@ -59,7 +58,7 @@ public class BlockCipherAlgorithm {
     /**
      * feistel untuk melakukan feistel setiap 16 byte
      */
-    public void feistel()
+    public void feistel() throws UnsupportedEncodingException
     {
         // inisialisasi right left
         byte [] Left = new byte[init.length / 2];
@@ -68,7 +67,7 @@ public class BlockCipherAlgorithm {
         System.arraycopy(this.init, init.length / 2 , Right, 0, init.length / 2);
         
         // Mulai feistel per 16 byte
-        for(int i = 0; i < 2; i++)
+        for(int i = 0; i < 8; i++)
         {
             // simpan initial state
             byte [] LeftTemp = new byte[Left.length];
@@ -81,6 +80,46 @@ public class BlockCipherAlgorithm {
             
             // R = L-1  XOR  R-1
             // Tambahkan fungsi F ke sini
+            RightTemp = ClassicCipherTools.encryptByte(RightTemp, keyhandler.getInternalKey(i));
+            RightTemp = Tools.getShuffled(this.key, RightTemp);
+            for(int idxbyte = 0; idxbyte < Right.length; idxbyte++)
+            {
+                byte temp = (byte) (LeftTemp[idxbyte] ^ RightTemp[idxbyte]);
+                Right[idxbyte] = temp;
+            }
+        }
+        
+        // Gabungkan ke array result, dan diswap right left nya
+        this.result = new byte[this.init.length];
+        System.arraycopy(Right, 0, this.result, 0, Right.length);
+        System.arraycopy(Left, 0, this.result, Right.length, Right.length);
+        
+        System.err.println(Tools.bytesToString(this.result));
+    }
+    
+    public void feisteldecrypt() throws UnsupportedEncodingException
+    {
+        // inisialisasi right left
+        byte [] Left = new byte[init.length / 2];
+        byte [] Right = new byte[init.length / 2];
+        System.arraycopy(this.init, 0, Left, 0, init.length / 2);
+        System.arraycopy(this.init, init.length / 2 , Right, 0, init.length / 2);
+        
+        // Mulai feistel per 16 byte
+        for(int i = 0; i < 8; i++)
+        {
+            // simpan initial state
+            byte [] LeftTemp = new byte[Left.length];
+            System.arraycopy(Left, 0, LeftTemp, 0, Left.length);
+            byte [] RightTemp = new byte[Right.length];
+            System.arraycopy(Right, 0, RightTemp, 0, Right.length);
+            
+            // L = R-1
+            System.arraycopy(Right, 0, Left, 0, Right.length);
+            
+            // R = L-1  XOR  R-1
+            // Tambahkan fungsi F ke sini
+            RightTemp = ClassicCipherTools.encryptByte(RightTemp, keyhandler.getInternalKey(7 - i));
             RightTemp = Tools.getShuffled(this.key, RightTemp);
             for(int idxbyte = 0; idxbyte < Right.length; idxbyte++)
             {
